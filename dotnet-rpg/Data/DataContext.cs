c2434dcd11137836faa406017287e78067840abe
Leoyu <407382380@qq.com>
2020-05-28 23:20:49 +0800

    asp.net core entityframecore

---
+++ dotnet-rpg/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Character> characters{get;set;}
+    }
+}
